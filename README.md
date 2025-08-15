10c553708021c09115cc59a1eef1740dab03600c
Solomon Chan <solomonchan.2023@scis.smu.edu.sg>
2025-08-15 17:28:51 +0800

    added api_call feature

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Stock-Analyzer-with-Mean-Reversion-Strategy
 #### Video Demo:
 #### Description: A stock analyzer that implements a mean reversion strategy using Bollinger Bands and RSI to execute and simulate trades over a period of time.
+
+
+####    Future improvement: 
+#####   - Extend to multiple tickers and portfolio-level backtesting.
--- project.py
+++ project.py
@@ -1,9 +1,26 @@
+import requests
+import json
+
+
+APIKEY = "8RUS0YURXTPQQYEM"
+
+
 def main():
-    ...
+    # Alpha Vantage API Key (Solomon)
+
+    # Get Ticker, Initial Capital & Time
+    ticker = input("Ticker: ")
+    amount = int(input("Initial Amount: "))
+    time_length = int(input("Length of time (days): "))
+
+    stock_info = api_call(ticker)
+    print(json.dumps(stock_info, indent=2))
 
 
-def function_1():
-    ...
+def api_call(ticker):
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={APIKEY}"
+    r = requests.get(url)
+    return r.json()
 
 
 def function_2():
